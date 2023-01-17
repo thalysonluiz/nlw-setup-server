4fa4a07a94a6b9b6625302d4ec664fbabde63301
Thalyson Luiz <thalysonluiz@gmail.com>
2023-01-17 15:31:12 -0300

    route get habits

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import Fastify from "fastify";
+import {PrismaClient} from '@prisma/client'
 
 const app = Fastify();
+const prisma = new PrismaClient();
 
-app.get('/', (req,res) => {
-  return ''
+app.get('/habits', async () => {
+  const habits = await prisma.habit.findMany();
+
+  return habits;
 })
 
 const port = 3333;
