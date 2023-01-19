a723e7af75adcec020456220417f82a3b258511c
Thalyson Luiz <thalysonluiz@gmail.com>
2023-01-19 14:30:03 -0300

    route create habit

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,17 @@
 import Fastify from "fastify";
-import {PrismaClient} from '@prisma/client'
-import cors from '@fastify/cors'
+import cors from "@fastify/cors";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors);
-
-app.get('/habits', async () => {
-  const habits = await prisma.habit.findMany();
-
-  return habits;
-})
+app.register(appRoutes);
 
 const port = 3333;
-app.listen({
-  port
-}).then(() => {
-  console.log('listening on port ' + port);
-});
+app
+  .listen({
+    port,
+  })
+  .then(() => {
+    console.log("listening on port " + port);
+  });
